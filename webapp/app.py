b97dc472063ab4acb20490479ec3ef5710854163
Aakash Singh <17aakashsingh1999@gmail.com>
2019-03-21 17:58:11 +0530

    web app almost ready

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request, render_template
+import json
 
 app = Flask(__name__)
 app.debug = True
@@ -8,13 +9,29 @@ app.debug = True
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    with open('headers.json', 'r') as f:
+        tables = json.load(f)
+    context = {
+        'tables': [{'id': tab, 'header': tables[tab]} for tab in tables.keys()],
+    }
+    return render_template('index.html', context=context)
 
-@app.route("/result", methods=['POST'])
+@app.route("/results", methods=['POST'])
 def result():
-    data = request.form
-    # load the model and do the processing here
-    payload = request.form
-    return render_template('result.html', payload=payload)
+    question = request.form['question']
+    table_id = request.form['id']
+    
+    context = request.form
+    return render_template('result.html', context=context)
+
+@app.route("/table/<string:table_id>")
+def table_id(table_id):
+    with open('questions.json', 'r') as f:
+        data = json.load(f)
+    context = {
+        'id': table_id,
+        'questions': data[table_id]
+    }
+    return render_template('questions.html', context=context)
 
 app.run()
